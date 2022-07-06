b1386a3ccc4131808beadeb677150f943a05a766
Teten Nugraha <teten.nugraha18@gmail.com>
2022-07-06 13:56:22 +0700

    Add error page

--- pages/about.js
+++ pages/about.js
@@ -2,19 +2,25 @@ import Link from 'next/link';
 import fetch from "isomorphic-unfetch";
 import Layout from '../components/Layout';
 import { Component } from 'react';
+import Error from './_error';
 
 export default class About extends Component {
 
   static async getInitialProps() {
     const res = await fetch("https://api.github.com/users/teten-nugraha");
+    const statusCode = res.status > 200 ? res.status : false;
     const data = await res.json();
 
-    return { user : data };
+    return { user : data, statusCode };
   }
 
   render() {
 
-    const { user } = this.props;
+    const { user, statusCode } = this.props;
+
+    if(statusCode) {
+      return <Error statusCode={statusCode} />
+    }
 
     return (
       <Layout title="About Me">
